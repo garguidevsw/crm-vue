6afc9fb59c67f9a2ef31760b3eee2d9f38739071
garguidevsw <garguidevsw@gmail.com>
2023-07-30 16:23:30 -0600

    Init Project Vue + VueRouter + TailwindCSS

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Inicio from '../views/ClientesView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'inicio',
+      component: Inicio
+    }
+  ]
+})
+
+export default router
