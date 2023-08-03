a969dc4afd11ae2e43e2b447f567815b66323401
garguidevsw <garguidevsw@gmail.com>
2023-08-03 12:57:43 -0600

    Add Json Server

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -8,6 +8,11 @@ defineProps({
     type: String,
   },
 });
+
+const handleSubmit = (data) => {
+
+}
+
 </script>
 
 <template>
@@ -19,16 +24,61 @@ defineProps({
 
     <div class="mx-auto mt-10 bg-white shadow">
       <div class="mx-auto md:w-2/3 py-20 px-6">
-        <FormKit type="form">
+        <FormKit 
+          type="form"
+          submit-label="Agregar Cliente"
+          incomplete-message="No se pudo enviar el formulario, revisa los mensajes de error!"
+          @submit="handleSubmit"
+        >
           <FormKit
             type="text"
+            name="nombre"
             label="Nombre"
             placeholder="Nombre del cliente"
-            help="Coloca el nombre del cliente que deseas registrar"
             validation="required"
             :validation-messages="{ required: 'El nombre del cliente es obligatorio' }"
           />
+          <FormKit
+            type="text"
+            name="apellido"
+            label="Apellido"
+            placeholder="Apellido del cliente"
+            validation="required"
+            :validation-messages="{ required: 'El apellido del cliente es obligatorio' }"
+          />
+          <FormKit
+            type="email"
+            name="email"
+            label="Correo Electrónico"
+            placeholder="Correo electrónico del cliente"
+            validation="required|email"
+            :validation-messages="{ required: 'El correo electrónico del cliente es obligatorio', email: 'Coloca un correo electrónico válido' }"
+          />
+          <FormKit
+            type="text"
+            name="telefono"
+            label="Teléfono"
+            placeholder="Teléfono: XXX-XXX-XXXX"
+            validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages="{ matches: 'El formato no es válido' }"
+          />
+
+          <FormKit
+            type="text"
+            name="empresa"
+            label="Empresa"
+            placeholder="Empresa del cliente"
+          />
+
+          <FormKit
+            type="text"
+            name="puesto"
+            label="Puesto"
+            placeholder="Puesto del cliente"
+          />
+
         </FormKit>
+
       </div>
     </div>
   </div>
